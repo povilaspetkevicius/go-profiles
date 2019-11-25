ca388fa4f2e5b26c84b73eab77b68ab69b3f5067
povilaspetkevicius <p.petkevicius200@gmail.com>
2019-11-25 23:06:58 +0200

    bit of progress

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -15,13 +16,15 @@ import (
 )
 
 var client *mongo.Client
-var profileCollection *mongo.Collection = client.Database("kimchi").Collection("profile")
+
+// var profileCollection *mongo.Collection = *mongo.Client.Database("kimchi").Collection("profile")
 
 // Profile - yet to be defined
 type Profile struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
 	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	Age       int                `json:"age,omitempty" bson:"age,omitempty"`
 }
 
 // CreateProfile - create a single profile
@@ -29,34 +32,62 @@ func CreateProfile(resp http.ResponseWriter, request *http.Request) {
 	resp.Header().Add("content-type", "application/json")
 	var profile Profile
 	err := json.NewDecoder(request.Body).Decode(&profile)
+
+	profileCollection := client.Database("kimchi").Collection("profile")
+
 	if err != nil {
 		log.Panicf("Could not decode request: %v", err)
 		return
 	}
-	insertResult, err := profileCollection.InsertOne(context.TODO(), 2*time.Second)
+	insertResult, err := profileCollection.InsertOne(context.TODO(), profile)
 	if err != nil {
 		log.Panicf("Failed to insert document into collection: %v", err)
 	} else {
 		log.Printf("Successfully inserted document into collection, id: %v", insertResult.InsertedID)
+		json.NewEncoder(resp).Encode(insertResult)
 	}
 }
 
 // GetAllProfiles - return all documents from profile collection - TODO finish up
 func GetAllProfiles(resp http.ResponseWriter, request *http.Request) {
+	profileCollection := client.Database("kimchi").Collection("profile")
 	resp.Header().Add("context-type", "application/json")
-	// fetchResult, err := profileCollection.
+	cursor, err := profileCollection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Panicf("Could not get db objects: %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{ "message"` + err.Error() + `"}`))
+		return
+	} else {
+		resp.WriteHeader(http.StatusOK)
+
+	}
+	defer cursor.Close(context.TODO())
+
+	var profiles []Profile
+	for cursor.Next(context.TODO()) {
+		var profile Profile
+		cursor.Decode(&profile)
+		profiles = append(profiles, profile)
+	}
+	if err := cursor.Err(); err != nil {
+		log.Panicf("Could not parse db objects: %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{ "message"` + err.Error() + `"}`))
+	}
+	json.NewEncoder(resp).Encode(profiles)
 }
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Print("Client initialized")
-	}
+	log.Println("Start of program execution")
+	client, _ = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
 	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
 
-	err = client.Connect(ctx)
+	err := client.Connect(ctx)
+
+	defer client.Disconnect(context.TODO())
+
 	if err != nil {
 		log.Fatal(err)
 	} else {
@@ -67,6 +98,7 @@ func main() {
 		log.Fatalf("Unable to connect to mongo: %v", err)
 	}
 	router := mux.NewRouter()
-	router.HandleFunc("/profile", CreateProfile).Methods("POST")
+	router.HandleFunc("/profiles", CreateProfile).Methods("POST")
+	router.HandleFunc("/profiles", GetAllProfiles).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
